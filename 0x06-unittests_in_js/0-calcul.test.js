cc6ace7fa970ccc6db5c95e341ed5afcc57760ea
mesh-254 <meshack1995@gmail.com>
2022-08-11 20:34:10 +0300

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,36 @@
-calculateNumber = require('./0-calcul');
 const assert = require('assert');
+const calculateNumber = require("./0-calcul.js");
 
-describe('calculateNumber', function () {
-    it('should return the sum of two numbers', () => {
-        assert(calculateNumber(1, 2), 3);
-    });
-    it('should return the sum of two numbers', function () {
-        assert(calculateNumber(1.5, 2.5), 4);
-    });
-    it('rounding up a\'s floating point fractional numbers', () => {
-        assert.strictEqual(calculateNumber(2.6, 2.0), 5);
-    });
-    it ('rounding up b\'s floating point fractional numbers', () => {
-        assert.strictEqual(calculateNumber(-2.0, -2.6), -5);
-    });
-})
+describe('calculateNumber', () => {
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
+  });
+
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
+  });
+
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
+
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
+
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
+
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
+
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
+
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
+  });
+});
